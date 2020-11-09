4945c4bafec5f887a9ec76eab577c0a876ddbe2e
pancreasio <julianholisu@gmail.com>
2020-11-09 20:28:40 -0300

    input system remade

--- Parry-This/Assets/Scripts/InputManager.cs
+++ Parry-This/Assets/Scripts/InputManager.cs
@@ -8,52 +8,19 @@ public class InputManager : MonoBehaviour
     public static LevelManager.GameplayEvent OnAttackEnd;
     public static LevelManager.GameplayEvent OnDefendStart;
     public static LevelManager.GameplayEvent OnDefendEnd;
-    // Start is called before the first frame update
-    void Start()
-    {
 
+    public void Attack()
+    {
+        LevelManager.InvokeIfNotNull(OnAttackStart);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void BeginDefend()
     {
-#if UNITY_ANDROID && !UNITY_EDITOR
-        if (Input.touchCount > 0)
-        {
-            Touch touch = Input.GetTouch(0);
-            if (touch.phase == TouchPhase.Began)
-            {
-                if (Camera.main.ScreenToViewportPoint(touch.position).x < 0.5f && Camera.main.ScreenToViewportPoint(touch.position).y < 0.7f )
-                    LevelManager.InvokeIfNotNull(OnDefendStart);
-                else
-                    LevelManager.InvokeIfNotNull(OnAttackStart);
-            }
-            else
-            if (touch.phase == TouchPhase.Ended)
-            {
-                if (Camera.main.ScreenToViewportPoint(touch.position).x < 0.5f)
-                    LevelManager.InvokeIfNotNull(OnDefendEnd);
-                else
-                    LevelManager.InvokeIfNotNull(OnAttackStart);
-            }
-        }
-#endif
+        LevelManager.InvokeIfNotNull(OnDefendStart);
+    }
 
-#if UNITY_EDITOR
-        if (Input.GetMouseButtonDown(0))
-        {
-            if (Camera.main.ScreenToViewportPoint(Input.mousePosition).x < 0.5f)
-                LevelManager.InvokeIfNotNull(OnDefendStart);
-            else
-                LevelManager.InvokeIfNotNull(OnAttackStart);
-        }
-        if (Input.GetMouseButtonUp(0))
-        {
-            if (Camera.main.ScreenToViewportPoint(Input.mousePosition).x < 0.5f)
-                LevelManager.InvokeIfNotNull(OnDefendEnd);
-            else
-                LevelManager.InvokeIfNotNull(OnAttackEnd);
-        }
-#endif
+    public void EndDefend()
+    {
+        LevelManager.InvokeIfNotNull(OnDefendEnd);
     }
 }
